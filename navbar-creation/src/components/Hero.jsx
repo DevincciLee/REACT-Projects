f3e933a37ca9bc13381f7e1e02ec4d95ff61718c
vinnyR. <163418223+DevincciLee@users.noreply.github.com>
2025-10-03 04:37:21 +0000

    created hero

---
+++ navbar-creation/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='bg-[#808080] flex flex-col justify-around items-center h-175 max-w-[80%] mx-auto text-center rounded-xl mt-10'>
+        <h1>Try</h1>
+        <h1>Try</h1>
+        <h1>Try</h1>
+        <h1>Try</h1>
+
+    </div>
+  )
+}
+
+export default Hero
